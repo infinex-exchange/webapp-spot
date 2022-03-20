0fc80efe7a7372332e573ef2c09d05ea4fbafd05
vayamos-exchange <admin@vayamos.cc>
2022-03-20 13:41:27 +0100

    Withdrawal module

--- wallet/withdraw.js
+++ wallet/withdraw.js
@@ -2,11 +2,11 @@ $(document).ready(function() {
     window.renderingStagesTarget = 2;
     
     $('#select-coin').on('change', function() {
-        $('#deposit-step2').hide();
-        $('#deposit-step3').hide();
+        $('#withdraw-step2').hide();
+        $('#withdraw-step3').hide();
         var asset = $('#select-coin').val();
         $.ajax({
-            url: config.apiUrl + '/wallet/deposit',
+            url: config.apiUrl + '/wallet/withdraw/networks',
             type: 'POST',
             data: JSON.stringify({
                 api_key: window.apiKey,
@@ -21,11 +21,10 @@ $(document).ready(function() {
                 window.recentTxAS.data.asset = asset;
                 window.recentTxAS.reset();
                 
-                window.depositInfo = data.deposit;
                 $('#select-net').val('');
                 $('#select-net').attr('data-network', '');
                 $('#select-net-data').html('');
-                $.each(data.deposit, function(k, v) {
+                $.each(data.networks, function(k, v) {
                     $('#select-net-data').append(`
                         <div class="select-net-item row p-1 hoverable" data-network="${k}" data-description="${v.description}">
                             <div class="col-auto my-auto text-center" style="width: 32px">
@@ -44,7 +43,7 @@ $(document).ready(function() {
                     $('#select-net-dropdown').toggle();
                     $('#select-net-arrow').toggleClass('flip');
                 });
-                $('#deposit-step2').show();
+                $('#withdraw-step2').show();
             } else {
                 msgBox(data.error);
             }
@@ -54,32 +53,57 @@ $(document).ready(function() {
         });
     });
     
-    $('#select-net').on('change', function() {
-        $('#deposit-step3').hide();
-        var di = window.depositInfo[$(this).attr('data-network')];
-        $('#deposit-addr').html(di.address);
-        window.qrcode.clear();
-        window.qrcode.makeCode(di.address);
-        $('#deposit-step3').show();
+    $('#select-net').on('change', function() {        
+        $('#withdraw-step3').show();
+    });
+    
+    $('#withdraw-address').on('input', function() {
+        if(typeof(window.addrTypingTimeout) !== 'undefined')
+            clearTimeout(window.addrTypingTimeout);
+        window.addrTypingTimeout = setTimeout(function() {
+            
+            $.ajax({
+                url: config.apiUrl + '/wallet/withdraw/validate',
+                type: 'POST',
+                data: JSON.stringify({
+                    api_key: window.apiKey,
+                    asset: $('#select-coin').val(),
+                    network: $('#select-net').attr('data-network'),
+                    address: $('#withdraw-address').val()
+                }),
+                contentType: "application/json",
+                dataType: "json",
+            })
+            .retry(config.retry)
+            .done(function (data) {
+                if(!data.success) {
+                    msgBox(data.error);
+                }
+                else if(!data.valid) {
+                    $('#help-address').show();
+                }
+                else {
+                    $('#help-address').hide();
+                }
+            })
+            .fail(function (jqXHR, textStatus, errorThrown) {
+                msgBoxNoConn(false);
+            });
+            
+        }, 750);
     });
 });
 
 $(document).on('authChecked', function() {
     if(window.loggedIn) {
-        window.qrcode = new QRCode("deposit-qrcode", {
-            width: 128,
-            height: 128,
-            correctLevel : QRCode.CorrectLevel.H
-        });
-    
         var rtxData = {
             api_key: window.apiKey,
-            type: 'DEPOSIT',
+            type: 'WITHDRAWAL',
         };
         
         var pathArray = window.location.pathname.split('/');
         var pathLast = pathArray[pathArray.length - 1];
-        if(pathLast != 'deposit' && pathLast != '') {
+        if(pathLast != 'withdraw' && pathLast != '') {
             var symbol = pathLast.toUpperCase();
             $.ajax({
                 url: config.apiUrl + '/wallet/assets',
---
+++ wallet/withdraw.php
@@ -0,0 +1,86 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <?php include('../templates/head.php'); ?>
+        <script src="/js/ajax_scroll.js"></script>
+    </head>
+    <body>
+    
+        <!-- Preloader -->
+        <?php include('../templates/preloader.html'); ?>
+        
+        <!-- Navbar -->
+        <?php include('../templates/navbar.html'); ?>
+        
+        <!-- Root container -->
+        <div id="root" class="container-fluid container-1500 pt-2 user-only">
+        <div class="row m-0">
+        
+        <!-- Main column -->
+        <div class="col-12 col-lg-9 p-0 ui-card ui-column">
+        
+            <div class="row p-2">
+                <h1>Withdraw</h1>
+            </div>
+            
+            <div class="row p-2">
+                <h3>&#9312 Select coin to withdraw:</h3>
+                <?php include('../templates/select_coin.html'); ?>
+            </div>
+            
+            <div id="withdraw-step2" style="display: none">
+                <div class="row p-2">
+                    <h3>&#9313 Select withdrawal network:</h3>
+                    <?php include('../templates/select_net.html'); ?>
+                </div>
+            </div>
+            
+            <div id="withdraw-step3" style="display: none">
+                <div class="row p-2">
+                    <h3>&#9314 Complete withdrawal:</h3>
+                    <form class="d-grid gap-3">
+                        <div class="form-group">
+                            <label for="withdraw-address">Address:</label>
+                            <input type="text" class="form-control" id="withdraw-address">
+                            <small id="help-address" class="form-text" style="display: none">Address is invalid</small>
+                        </div>
+                        <div class="form-group">
+                            <label for="withdraw-amount">Amount:</label>
+                            <input type="text" class="form-control" id="withdraw-amount">
+                        </div>
+                        <div class="form-group">
+                            <label for="withdraw-fee">Fee:</label>
+                            <input type="text" class="form-control" id="withdraw-fee">
+                        </div>
+                        <button type="submit" class="btn btn-primary">Submit</button>
+                    </form>
+                </div>
+            </div>
+        
+        <!-- / Main column -->
+        </div>
+        
+        <!-- Right column -->
+        <div class="col-12 col-lg-3 p-0 ui-card ui-column">
+        
+            <div class="row p-2">
+                <h1>Last withdrawals</h1>
+            </div>
+            
+            <div id="recent-tx-data">
+            </div>
+        
+        <!-- / Right column -->
+        </div>
+            
+        <!-- / Root container -->    
+        </div>
+        </div>
+        
+        <script src="/js/recent_tx.js"></script>
+        <script src="/wallet/withdraw.js"></script>
+        
+        <?php include('../templates/modals.html'); ?>
+    
+    </body>
+</html>
