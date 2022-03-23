5cd0719792794b89d72a2404d28e14528b384588
vayamos-exchange <admin@vayamos.cc>
2022-03-23 14:57:10 +0100

    Mobile menu from Imperial Casino

--- templates/sidebar_account.php
+++ templates/sidebar_account.php
@@ -5,4 +5,25 @@
     </div>
             
     <?php include('menu_account.html'); ?>
+    
 </div>
+
+<nav class="navbar fixed-bottom navbar-expand navbar-mobile d-flex d-lg-none py-0">
+    <ul class="navbar-nav mx-auto text-center">
+        <li class="nav-item">
+            <a class="nav-link" href="/">
+                <i class="fas fa-home"></i><br>
+                Lobby
+            </a>
+        </li>
+        <li class="nav-item dropup">
+            <a class="nav-link dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false">
+                <i class="fas fa-info"></i><br>
+                Informations
+            </a>
+            <div class="dropdown-menu">
+                <?php include('menu_account.html'); ?>
+            </div>
+        </li>
+    </ul>
+</nav>
