3e8475ac2c0174b630591db3a473009c31eafcc1
vayamos-exchange <admin@vayamos.cc>
2022-03-26 16:59:53 +0100

    Started working on open orders

--- exchange/spot/js/orders.js
+++ exchange/spot/js/orders.js
@@ -2,57 +2,87 @@ $(document).on('authChecked pairSelected', function() {
     if(typeof(window.multiEvents['authChecked']) == 'undefined' || typeof(window.multiEvents['pairSelected']) == 'undefined') return;
     
     if(window.loggedIn) {
-        $.ajax({
-            url: config.apiUrl + '/spot/open_orders',
-            type: 'POST',
-            data: JSON.stringify({
-                api_key: window.apiKey
-            }),
-            contentType: "application/json",
-            dataType: "json",
-        })
-        .done(function (data) {
-            if(data.success) {    
-                $(data.orders).each(function() {
-                    var time = new Date(this.time).toLocaleTimeString();
-                    $('#orders-open-data').append(`
-                        <div class="row">
-                            <div class="col">
-                                ${time}
-                            </div>
-                            <div class="col">
-                                ${this.pair}
-                            </div>
-                            <div class="col">
-                                ${this.type}
-                            </div>
-                            <div class="col">
-                                ${this.side}
-                            </div>
-                            <div class="col">
-                                ${this.price}
-                            </div>
-                            <div class="col">
-                                ${this.amount}
-                            </div>
-                            <div class="col">
-                                ${this.filled}
-                            </div>
-                            <div class="col">
-                                ${this.total}
-                            </div>
-                            <div class="col">
-                                <i class="fa-solid fa-xmark" onClick="alert(1)"></i>
-                            </div>
-                        </div>
-                    `);
-                });
+        window.openOrdersAS = new AjaxScroll(
+            $('#orders-open-data'),
+            $('#orders-open-preloader'),
+            {
+                api_key: window.apiKey,
+                sort_pair: window.currentPair
+            },
+            function() {
+                this.data.offset = this.offset;
+                var thisAS = this;
             
+            //---
+    $.ajax({
+        url: config.apiUrl + '/spot/open_orders',
+        type: 'POST',
+        data: JSON.stringify(thisAS.data),
+        contentType: "application/json",
+        dataType: "json",
+    })
+    .retry(config.retry)
+    .done(function (data) {
+        if(data.success) {
+            $(data.orders).each(function() {
+                var time = new Date(this.time).toLocaleTimeString();
+                $('#orders-open-data').append(`
+                    <div class="row">
+                        <div class="col">
+                            ${time}
+                        </div>
+                        <div class="col">
+                            ${this.pair}
+                        </div>
+                        <div class="col">
+                            ${this.type}
+                        </div>
+                        <div class="col">
+                            ${this.side}
+                        </div>
+                        <div class="col">
+                            ${this.price}
+                        </div>
+                        <div class="col">
+                            ${this.amount}
+                        </div>
+                        <div class="col">
+                            ${this.filled}
+                        </div>
+                        <div class="col">
+                            ${this.total}
+                        </div>
+                        <div class="col">
+                            <i class="fa-solid fa-xmark" onClick="alert(1)"></i>
+                        </div>
+                    </div>
+                `);
+            });
+            
+            thisAS.done();
+            
+            if(thisAS.offset == 0)
                 $(document).trigger('renderingStage'); // 5
-            }
-        })
-        .fail(function (jqXHR, textStatus, errorThrown) {  
-        });
+            
+            if(data.orders.length != 25)
+                thisAS.noMoreData(); 
+        }
+        else {
+            msgBoxRedirect(data.error);
+            thisAS.done();
+            thisAS.noMoreData();
+        }
+    })
+    .fail(function (jqXHR, textStatus, errorThrown) {
+        msgBoxNoConn(true);
+        thisAS.done();
+        thisAS.noMoreData();  
+    });
+            //---
+        
+            },
+            true
+        );
         
         $.ajax({
             url: config.apiUrl + '/spot/orders_history',
