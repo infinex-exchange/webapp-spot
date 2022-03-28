07c6706f070c4c95f1262d9f3d39ff0f83d070aa
vayamos-exchange <admin@vayamos.cc>
2022-03-28 15:00:22 +0200

    Index: markets list

--- index.js
+++ index.js
@@ -1,4 +1,50 @@
 $(document).ready(function() {
     window.renderingStagesTarget = 1;
-    $(document).trigger('renderingStage');    
+    $(document).trigger('renderingStage');
+    
+    $.ajax({
+        url: config.apiUrl + '/spot/markets_ex',
+        type: 'POST',
+        data: JSON.stringify({
+            offset: 0
+        }),
+        contentType: "application/json",
+        dataType: "json",
+    })
+    .retry(config.retry)
+    .done(function (data) {
+        if(data.success) {
+            $.each(data.markets, function(k, v) {   
+                var color = '';
+                if(v.change > 0) color = 'text-success';
+                if(v.change < 0) color = 'text-danger';
+                
+                $('#market-trend-spot-data').append(`
+                    <div class="row p-2 hoverable">
+                        <div class="col-3 m-auto">
+                            <img width="32px" height="32px" src="${v.icon_url}">
+                            ${k}
+                        </div>
+                        <div class="col-3 m-auto text-end">
+                            ${v.price} ${v.quote}
+                        </div>
+                        <div class="col-3 m-auto text-end">
+                            <span class="${color}">
+                                ${v.change}%
+                            </span>
+                        </div>
+                        <div class="col-3 m-auto text-end">
+                            ${v.vol_quote} ${v.quote}
+                        </div>
+                    </div>
+                `);
+            }); 
+        }
+        else {
+            msgBox(data.error);
+        }
+    })
+    .fail(function (jqXHR, textStatus, errorThrown) {
+        msgBoxNoConn(false); 
+    });    
 });
--- index.php
+++ index.php
@@ -14,7 +14,7 @@
         <!-- Root container -->
         <div id="root" class="container-fluid container-1500 pt-2 container-rest">
         
-            <div class="row m-0 p-5">
+            <div class="row m-0 px-2 py-5">
                 <div class="jumbotron col-12">
                     <h1>Vayamos Exchange</h1>
                     <p>Official digital assets exchange of BPX ecosystem.</p>
@@ -25,16 +25,18 @@
             <div class="row">
                 <div class="col-12 p-4 ui-card-high rounded">
                 
-                    <div class="row text-center">
+                    <div class="row py-2 text-center">
                         <h1>Market trend</h1>
                     </div>
                     
-                    <div class="row">
-                        <div class="col">Name</div>
-                        <div class="col">Last price</div>
-                        <div class="col">24h change</div>
-                        <div class="col">Market cap</div>
+                    <div class="row text-hi py-2">
+                        <div class="col-3">Name</div>
+                        <div class="col-3 text-end">Last price</div>
+                        <div class="col-3 text-end">24h change</div>
+                        <div class="col-3 text-end">Market cap</div>
                     </div>
+                    
+                    <div id="market-trend-spot-data"></div>
                 
                 </div>
             </div>
