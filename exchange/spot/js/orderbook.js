603ab9c4696ac43b1a7fa3ccfb1cc9968afe3f3c
vayamos-exchange <admin@vayamos.cc>
2022-04-07 18:09:17 +0200

    Streaming: orderbook

--- exchange/spot/js/orderbook.js
+++ exchange/spot/js/orderbook.js
@@ -1,4 +1,4 @@
-function orderBookAppend(side, row) {
+function orderBookUpdate(side, row) {
     var bnPrice = new BigNumber(row.price);
     var bnAmount = new BigNumber(row.amount);
     var bnTotal = bnPrice.multipliedBy(bnAmount);
@@ -17,20 +17,56 @@ function orderBookAppend(side, row) {
     if(side == 'bid') div = $('#orderbook-buy');
     else div = $('#orderbook-sell');
     
-    div.append(`
-        <div class="row" data-side="${side}" data-price="${row.price}" onClick="orderBookClick(this)"
-             style="background-size: calc(${strTotal} / var(--orderbook-total-max) * 100%)">
-            <div class="col-4 ${color}">
-                ${row.price}
+    var existing = div.find('.row[data-price="' + row.price + '"]');
+    
+    if(row.amount == 0) {
+        if(existing.length) existing.remove();
+        return;
+    }
+    
+    if(existing.length) {
+        var price = existing.find('.price');
+        price.removeClass('text-green text-red');
+        price.addClass(color);
+        price.html(row.price);
+        existing.find('.amount').html(row.amount);
+        existing.find('.total').html(strTotal);
+    }
+    
+    else {
+        var html = `
+            <div class="row" data-side="${side}" data-price="${row.price}" onClick="orderBookClick(this)"
+                    style="background-size: calc(${strTotal} / var(--orderbook-total-max) * 100%)">
+                <div class="col-4 price ${color}">
+                    ${row.price}
+                </div>
+                <div class="col-4 amount text-end">
+                    ${row.amount}
+                </div>
+                <div class="col-4 total text-end">
+                    ${strTotal}
+                </div>
             </div>
-            <div class="col-4 text-end">
-                ${row.amount}
-            </div>
-            <div class="col-4 text-end">
-                ${strTotal}
-            </div>
-        </div>
-    `);
+        `;
+        
+        var inserted = false;    
+        div.children().each(function() {
+            if(!inserted
+               &&
+              (
+                side == 'bid' && bnPrice.lt($(this).attr('data-price'))
+                ||
+                side == 'ask' && bnPrice.gt($(this).attr('data-price'))
+              )
+             ) {
+                $(html).insertBefore($(this));
+                inserted = true;
+            }
+        });
+        
+        if(!inserted)
+            div.append(html);
+    }
 }
 
 function orderBookClick(row) {
@@ -62,12 +98,25 @@ $(document).on('pairSelected', function() {
             data.asks = data.asks.slice(-20).reverse();
               
             $(data.bids).each(function() {
-                orderBookAppend('bid', this);
+                orderBookUpdate('bid', this);
             });
             
             $(data.asks).each(function() {
-                orderBookAppend('ask', this);
+                orderBookUpdate('ask', this);
             });
+            
+            window.wsClient.sub(
+                window.currentPair + '@orderBook',
+                function(data) {
+                    if(data.side == 'BUY')
+                        orderBookUpdate('bid', data);
+                    else
+                        orderBookUpdate('ask', data);
+                },
+                function(error) {
+                    msgBoxRedirect(error);
+                }
+            );
             
             $(document).trigger('renderingStage'); // 7
         }
