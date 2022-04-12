b9730310c6ce5795e967194a472bd684b48b519d
vayamos-exchange <admin@vayamos.cc>
2022-04-12 13:23:30 +0200

    Fix orderbook background not updating when filling order

--- exchange/spot/js/orderbook.js
+++ exchange/spot/js/orderbook.js
@@ -1,13 +1,13 @@
 function orderBookUpdate(side, row) {
+    if(typeof(window.orderBookTotalMax) === 'undefined') {
+        window.orderBookTotalMax = new BigNumber(0);
+        document.body.style.setProperty('--orderbook-total-max', '0');
+    }
+    
     var bnPrice = new BigNumber(row.price);
     var bnAmount = new BigNumber(row.amount);
     var bnTotal = bnPrice.multipliedBy(bnAmount);
     var strTotal = bnTotal.toFixed(window.currentQuotePrecision);
-    
-    if(typeof(window.orderBookTotalMax) === 'undefined' || bnTotal.gt(window.orderBookTotalMax)) {
-        window.orderBookTotalMax = bnTotal;
-        document.body.style.setProperty('--orderbook-total-max', strTotal);
-    }
     
     var color = '';
     if(side == 'bid') color = 'text-green';
@@ -17,25 +17,30 @@ function orderBookUpdate(side, row) {
     if(side == 'bid') div = $('#orderbook-buy');
     else div = $('#orderbook-sell');
     
-    var existing = div.find('.row[data-price="' + row.price + '"]');
+    var existing = div.find('.orderbook-item[data-price="' + row.price + '"]');
     
-    if(row.amount == 0) {
-        if(existing.length) existing.remove();
-        return;
+    // Delete
+    if(existing.length && row.amount == 0) {
+        existing.remove();
     }
     
-    if(existing.length) {
+    // Update
+    else if(existing.length) {
         var price = existing.find('.price');
         price.removeClass('text-green text-red');
         price.addClass(color);
         price.html(row.price);
         existing.find('.amount').html(row.amount);
         existing.find('.total').html(strTotal);
+        existing.attr('data-total', strTotal);
+        existing.css({"background-size": `calc(${strTotal} / var(--orderbook-total-max) * 100%)`});
     }
     
+    // Add
     else {
         var html = `
-            <div class="row" data-side="${side}" data-price="${row.price}" onClick="orderBookClick(this)"
+            <div class="row orderbook-item" data-side="${side}" data-price="${row.price}" data-total="${strTotal}"
+                    onClick="orderBookClick(this)"
                     style="background-size: calc(${strTotal} / var(--orderbook-total-max) * 100%)">
                 <div class="col-4 price ${color}">
                     ${row.price}
@@ -67,6 +72,16 @@ function orderBookUpdate(side, row) {
         if(!inserted)
             div.append(html);
     }
+    
+    // Update total max
+    var newMax = new BigNumber(0);
+    $('.orderbook-item').each(function() {
+        var currentTotal = new BigNumber($(this).attr('data-total'));
+        if(currentTotal.gt(newMax))
+            newMax = currentTotal;
+    });
+    window.orderBookTotalMax = newMax;
+    document.body.style.setProperty('--orderbook-total-max', newMax.toFixed(window.currentQuotePrecision));
 }
 
 function orderBookClick(row) {
