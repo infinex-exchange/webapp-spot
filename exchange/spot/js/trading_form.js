5ebef42de0755d4c7d0f07de83794d1fa780c30b
vayamos-exchange <admin@vayamos.cc>
2022-04-12 12:28:24 +0200

    Revert "Trading form use safe_balances"
    This reverts commit 5a575346e5df170a0a691305b5f77363ea4dfcd9.

--- exchange/spot/js/trading_form.js
+++ exchange/spot/js/trading_form.js
@@ -14,7 +14,7 @@ function updateBalance() {
     
     if(window.loggedIn) {
         $.ajax({
-            url: config.apiUrl + '/spot/safe_balances',
+            url: config.apiUrl + '/wallet/balances',
             type: 'POST',
             data: JSON.stringify({
                 api_key: window.apiKey,
@@ -29,8 +29,8 @@ function updateBalance() {
         .retry(config.retry)
         .done(function (data) {
             if(data.success) {
-                window.currentBaseBalance = new BigNumber(data.safe_balances[window.currentBase]);
-                window.currentQuoteBalance = new BigNumber(data.safe_balances[window.currentQuote]);
+                window.currentBaseBalance = new BigNumber(data.balances[window.currentBase].avbl);
+                window.currentQuoteBalance = new BigNumber(data.balances[window.currentQuote].avbl);
                 updateBalanceAfter();
             }
             else {
