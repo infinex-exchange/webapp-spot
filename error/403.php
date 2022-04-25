d43020a90c8c25ae166116758a3a53bee1f0af54
vayamos-exchange <admin@vayamos.cc>
2022-04-25 15:18:24 +0200

    Error pages

---
+++ error/403.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <?php include('../templates/head.php'); ?>
+    </head>
+    <body class="vh-100 body-background">   
+        <!-- Navbar -->
+        <?php include('../templates/navbar.php'); ?>
+        <?php include('../templates/vanilla_mobile_nav.php'); ?>
+        
+        <!-- Root container -->
+        <div id="root" class="container-fluid container-1500 pt-2 container-rest">
+        <div class="row m-0 h-rest">
+        
+        <!-- Main column -->
+        <div class="col-12 col-lg-8 m-auto p-3 ui-card-high rounded">
+            
+            <h1>Error 403</h1>
+            <h3>You are not authorized to access this page.</h3>
+        
+        <!-- / Main column -->
+        </div>
+            
+        <!-- / Root container -->    
+        </div>
+        </div>
+    </body>
+</html>
