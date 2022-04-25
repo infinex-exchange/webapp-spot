cc3fc99ce9cef2f56566ac2fb0826c70d1b7e3dd
vayamos-exchange <admin@vayamos.cc>
2022-04-25 16:51:38 +0200

    Vanilla mobile nav missing small font

--- templates/vanilla_mobile_nav.php
+++ templates/vanilla_mobile_nav.php
@@ -1,4 +1,4 @@
-<nav class="navbar fixed-bottom navbar-expand navbar-mobile d-flex d-lg-none py-0">
+<nav class="navbar fixed-bottom navbar-expand navbar-mobile d-flex d-lg-none py-0 small">
     <ul class="navbar-nav mx-auto text-center">
         <?php include('mobile_menu_inner.php'); ?>
     </ul>
