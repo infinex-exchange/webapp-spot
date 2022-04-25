2f97ed172f1f4753d0df015b145d5935d78a1007
vayamos-exchange <admin@vayamos.cc>
2022-04-25 11:59:35 +0200

    Js sizing: simplify code

--- exchange/spot/js/js_sizing.js
+++ exchange/spot/js/js_sizing.js
@@ -1,24 +1,9 @@
 function doJsSizing() {
     document.body.style.setProperty('--height-status-bar', $('#status-bar').outerHeight() + 'px');
     document.body.style.setProperty('--height-mobile-nav', $('#mobile-navbar').outerHeight() + 'px');
-    
-    var tf = $('#trading-form');
-    if(tf.hasClass('d-none'))
-	    document.body.style.setProperty('--height-trading-form', tf.outerHeight() + 'px');
-	else
-		document.body.style.setProperty('--height-trading-form', '0px');
-	
-	var mh = $('#mobile-header');
-	if(mh.hasClass('d-none'))
-	    document.body.style.setProperty('--height-mobile-header', mh.outerHeight() + 'px');
-	else
-		document.body.style.setProperty('--height-mobile-header', '0px');
-	
-	var mn = $('#main-navbar');
-	if(mn.hasClass('d-none'))
-	    document.body.style.setProperty('--height-main-nav', mn.outerHeight() + 'px');
-	else
-		document.body.style.setProperty('--height-main-nav', '0px');
+	document.body.style.setProperty('--height-trading-form', $('#trading-form').outerHeight() + 'px');
+	document.body.style.setProperty('--height-mobile-header', $('#mobile-header').outerHeight() + 'px');
+	document.body.style.setProperty('--height-main-nav', $('#main-navbar').outerHeight() + 'px');
 }
 
 $(document).ready(function() {
