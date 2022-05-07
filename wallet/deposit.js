a570aa6758cb8f43df934f4d7a80b9587de615e1
vayamos-exchange <admin@vayamos.cc>
2022-05-07 16:41:05 +0200

    Wallet/withdraw: adjust to new api and new net selector

--- wallet/deposit.js
+++ wallet/deposit.js
@@ -2,63 +2,19 @@ $(document).ready(function() {
     window.renderingStagesTarget = 2;
     
     $('#select-coin').on('change', function() {
-        $('#deposit-step2').hide();
         $('#deposit-step3').hide();
-        var asset = $('#select-coin').val();
-        $.ajax({
-            url: config.apiUrl + '/wallet/deposit',
-            type: 'POST',
-            data: JSON.stringify({
-                api_key: window.apiKey,
-                asset: asset
-            }),
-            contentType: "application/json",
-            dataType: "json",
-        })
-        .retry(config.retry)
-        .done(function (data) {
-            if(data.success) {
-                window.recentTxAS.data.asset = asset;
-                window.recentTxAS.reset();
-                
-                window.depositInfo = data.deposit;
-                $('#select-net').val('');
-                $('#select-net').attr('data-network', '');
-                $('#select-net-data').html('');
-                $.each(data.deposit, function(k, v) {
-                    $('#select-net-data').append(`
-                        <div class="select-net-item row p-1 hoverable" data-network="${k}" data-description="${v.description}">
-                            <div class="col-auto my-auto text-center" style="width: 32px">
-                                <img width="24px" height="24px" src="${v.icon_url}">
-                            </div>
-                            <div class="col my-auto">
-                                ${v.description}
-                            </div>
-                        </div>
-                    `);
-                });
-                $('#select-net').trigger('dataReady');
-                $('.select-net-item').on('click', function() {
-                    $('#select-net').val($(this).attr('data-description'));
-                    $('#select-net').attr('data-network', $(this).attr('data-network'));
-                    $('#select-net').trigger('change');
-                });
-                $('#deposit-step2').show();
-            } else {
-                msgBox(data.error);
-            }
-        })
-        .fail(function (jqXHR, textStatus, errorThrown) {
-            msgBoxNoConn(false);
-        });
+        initSelectNet( $('#select-coin').val() );
+        $('#deposit-step2').show();
     });
     
     $('#select-net').on('change', function() {
         $('#deposit-step3').hide();
+        
         var di = window.depositInfo[$(this).attr('data-network')];
         $('#deposit-addr').html(di.address);
         window.qrcode.clear();
         window.qrcode.makeCode(di.address);
+        
         $('#deposit-step3').show();
         $('html, body').animate({
             scrollTop: $("#deposit-step3").offset().top
--- wallet/withdraw.js
+++ wallet/withdraw.js
@@ -2,15 +2,21 @@ $(document).ready(function() {
     window.renderingStagesTarget = 2;
     
     $('#select-coin').on('change', function() {
-        $('#withdraw-step2').hide();
         $('#withdraw-step3').hide();
-        var asset = $('#select-coin').val();
+        initSelectNet( $('#select-coin').val() );
+        $('#withdraw-step2').show();
+    });
+    
+    $('#select-net').on('change', function() {        
+        $('#withdraw-step3').hide();
+        
         $.ajax({
-            url: config.apiUrl + '/wallet/withdraw/networks',
+            url: config.apiUrl + '/wallet/withdraw/info',
             type: 'POST',
             data: JSON.stringify({
                 api_key: window.apiKey,
-                asset: asset
+                asset: $('#select-coin').val(),
+                network: $('#select-net').data('network')
             }),
             contentType: "application/json",
             dataType: "json",
@@ -18,31 +24,34 @@ $(document).ready(function() {
         .retry(config.retry)
         .done(function (data) {
             if(data.success) {
-                window.recentTxAS.data.asset = asset;
-                window.recentTxAS.reset();
+                // Operating warning
+                if(data.operating)
+                    $('#withdraw-operating-warning').addClass('d-none');
+                else
+                    $('#withdraw-operating-warning').removeClass('d-none');
                 
-                $('#select-net').val('');
-                $('#select-net').attr('data-network', '');
-                $('#select-net-data').html('');
-                $.each(data.networks, function(k, v) {
-                    $('#select-net-data').append(`
-                        <div class="select-net-item row p-1 hoverable" data-network="${k}" data-description="${v.description}">
-                            <div class="col-auto my-auto text-center" style="width: 32px">
-                                <img width="24px" height="24px" src="${v.icon_url}">
-                            </div>
-                            <div class="col my-auto">
-                                ${v.description}
-                            </div>
-                        </div>
-                    `);
-                });
-                $('#select-net').trigger('dataReady');
-                $('.select-net-item').on('click', function() {
-                    $('#select-net').val($(this).attr('data-description'));
-                    $('#select-net').attr('data-network', $(this).attr('data-network'));
-                    $('#select-net').trigger('change');
-                });
-                $('#withdraw-step2').show();
+                // Min and max fee
+                var feeStep = '0.';
+                for(var i = 0; i < data.prec - 1; i++)
+                    feeStep += '0';
+                feeStep += '1';
+                
+                $('#withdraw-fee').attr('min', data.fee_min)
+                                  .attr('max', data.fee_max)
+                                  .attr('step', feeStep);
+                
+                // Precision
+                
+                // Memo
+                if(typeof(data.memo_name) !== 'undefined')
+                    $('#withdraw-memo-wrapper').removeClass('d-none');
+                else
+                    $('#withdraw-memo-wrapper').addClass('d-none');
+                
+                $('#withdraw-step3').show();
+                $('html, body').animate({
+                    scrollTop: $("#withdraw-step3").offset().top
+                }, 1000);
             } else {
                 msgBox(data.error);
             }
@@ -50,13 +59,6 @@ $(document).ready(function() {
         .fail(function (jqXHR, textStatus, errorThrown) {
             msgBoxNoConn(false);
         });
-    });
-    
-    $('#select-net').on('change', function() {        
-        $('#withdraw-step3').show();
-        $('html, body').animate({
-            scrollTop: $("#withdraw-step3").offset().top
-        }, 1000);
     });
     
     $('#withdraw-address').on('input', function() {
