98f932aab323601910784ba9cbb5d472be829d60
vayamos-exchange <admin@vayamos.cc>
2022-05-10 00:55:11 +0200

    Withdraw: Reset form 2

--- wallet/withdraw.js
+++ wallet/withdraw.js
@@ -63,6 +63,8 @@ $(document).ready(function() {
                 
                 // Reset form
                 $('#withdraw-form').get(0).reset();
+                $('small[id^="help-"]').hide();
+                $('#withdraw-amount').val('').trigger('prevalidated');
                 
                 // Operating warning
                 if(data.operating)
