e6cc102149733b47656b02e4d9dacc14d8116f88
vayamos-exchange <admin@vayamos.cc>
2022-07-22 13:45:51 +0200

    Announcements: Add columns and feature img

--- info/announcements.js
+++ info/announcements.js
@@ -24,11 +24,27 @@ $(document).ready(function() {
                         $.each(data.announcements, function(k, v) {
                             var markdown = marked.parse(v.body);
                             var time = new Date(v.time * 1000).toLocaleString();
+                            
+                            var leftColConf = 'd-none';
+                            var rightColConf = 'col-12';
+                            var featureImg = 'data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==';
+                            
+                            if(typeof(data.feature_img) != 'undefined') {
+                                leftColConf = 'col-2';
+                                rightColConf = 'col-10';
+                                featureImg = data.feature_img;
+                            }
+                            
                             thisAS.append(`
                                 <div class="row mt-0 mb-4 p-2 ui-card-light">
-                                    <h3 class="pt-3">${v.title}</h3>
-                                    <h6 class="pb-2">${time}</h6>
-                                    <div class="secondary">` + markdown + `</div>
+                                    <div class="${leftColConf}">
+                                        <img src="${featureImg}" class="img-fluid m-auto">
+                                    </div>
+                                    <div class="${rightColConf}">
+                                        <h3 class="pt-3">${v.title}</h3>
+                                        <h6 class="pb-2">${time}</h6>
+                                        <div class="secondary">${markdown}</div>
+                                    </div>
                                 </div>
                             `);
                         });
