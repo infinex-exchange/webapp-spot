1766936dc69ff9de8e8cf25a040834ab9f0ac211
vayamos-exchange <admin@vayamos.cc>
2022-07-22 14:54:26 +0200

    Styling announcement

--- info/announcements.js
+++ info/announcements.js
@@ -48,7 +48,9 @@ $(document).ready(function() {
                             thisAS.append(`
                                 <div class="row mt-0 mb-4 p-2 ui-card-light">
                                     <div class="${leftColConf} m-auto">
-                                        <img src="${featureImg}" style="max-height: 100px; width: 100%;">
+                                        <div class="w-100 h-50">
+                                            <img src="${featureImg}" class="img-fluid">
+                                        </div>
                                     </div>
                                     <div class="${rightColConf}">
                                         <h3 class="pt-3">${v.title}</h3>
