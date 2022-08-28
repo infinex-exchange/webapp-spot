298b0a33d4dee3f1b7baa0ff803700802f8be918
vayamos-exchange <admin@vayamos.cc>
2022-08-29 00:02:54 +0200

    Scroll to negative big numBer

--- exchange/spot/js/js_sizing.js
+++ exchange/spot/js/js_sizing.js
@@ -28,7 +28,7 @@ function doJsSizing() {
     document.body.style.setProperty('--target-height-orders-data', remainH + 'px');
     
     $('#orderbook-sell').scrollTop(999999);
-    $('#orderbook-buy').scrollTop(0);
+    $('#orderbook-buy').scrollTop(-999999);
 }
 
 $(document).on('renderingComplete', function() {
