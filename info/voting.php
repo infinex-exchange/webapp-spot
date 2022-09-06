4bbabaaa361a6aefc592b8a59826bef98bd2ea4f
vayamos-exchange <admin@vayamos.cc>
2022-09-07 00:51:17 +0200

    Add text

--- info/voting.php
+++ info/voting.php
@@ -24,14 +24,24 @@
         <div class="col-12 col-lg-9 p-0 ui-card ui-column">
         
             <div class="row p-2">
-                <div class="col-auto my-auto">
+                <div class="col-12 col-lg-auto order-1 my-auto">
                     <h2>Vote for listing</h2>
                 </div>
-                <div class="user-only col-auto ms-0 ms-lg-auto my-auto">
+                <div class="user-only col-12 col-lg-auto ms-0 ms-lg-auto order-3 order-lg-2 my-auto">
                     <a href="#_" class="submit-project btn btn-primary">
                         <i class="fa-solid fa-plus"></i>
                         Submit project proposal
                     </a>
+                </div>
+                <div class="col-12 order-2 order-lg-3">
+                    BPX holders decide what projects will be listed on Vayamos.
+                    When you submit a listing proposal, it will be verified by the exchange administrator.
+                    If the project does not raise any doubts and meets our technical requirements,
+                    it will be included in the next voting.
+                    Each voting begins at midnight on the first day of each month and continues until the end of the month.
+					The project with the highest number of votes will be listed within a few days of the end of voting.
+					The number of your votes depends on the amount of BPX held.
+					You can split your votes among several projects.
                 </div>
             </div>
             
