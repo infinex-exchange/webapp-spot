3cbb904f726dae01a4c59f1a08cdc15336863af6
vayamos-exchange <admin@vayamos.cc>
2022-09-07 01:23:01 +0200

    Include validate js

--- info/voting.php
+++ info/voting.php
@@ -2,6 +2,7 @@
 <html lang="en">
     <head>
         <?php include('../templates/head.php'); ?>
+        <script src="/js/validate.js?<?php echo filemtime(__DIR__.'/../js/validate.js'); ?>"></script>
         <script src="/js/ajax_scroll.js?<?php echo filemtime(__DIR__.'/../js/ajax_scroll.js'); ?>"></script>
         <title>Vote for listing | Vayamos Exchange</title>
     </head>
