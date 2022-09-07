4e18d58ce39068f2b32516a5ff55f1cadfebea0a
vayamos-exchange <admin@vayamos.cc>
2022-09-07 11:15:20 +0200

    Hide modal after submit proposal

--- info/voting.js
+++ info/voting.js
@@ -97,6 +97,7 @@ $(document).ready(function() {
         .retry(config.retry)
         .done(function (data) {
             if(data.success) {
+                $('#modal-submit').modal('hide');
                 msgBox('The proposal has been sent and will appear in the next vote after being verified.');
             }
             else {
