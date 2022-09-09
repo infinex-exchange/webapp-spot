dd12db089dab09298baf5bf961ce878d6a9d9096
vayamos-exchange <admin@vayamos.cc>
2022-09-09 22:19:50 +0200

    Fix Wallet overview out of screen

--- wallet/overview.php
+++ wallet/overview.php
@@ -37,17 +37,17 @@
             
             <form>
             <div class="row p-2 flex-nowrap">
-                <div class="col-auto my-auto px-1">
+                <div class="col-12 col-lg-auto auto my-auto px-1">
                     <a href="/wallet/deposit" class="btn btn-primary btn-sm">Deposit</a>
                     <a href="/wallet/withdraw" class="btn btn-primary btn-sm">Withdraw</a>
                     <a href="/wallet/transfer" class="btn btn-primary btn-sm">Transfer</a>
                 </div>
                 
-                <div class="col-auto my-auto px-1">
+                <div class="col-6 col-lg-auto my-auto px-1">
                     <input id="asset-search" type="text" size="7" placeholder="Search" class="form-control input-search">
                 </div>
                 
-                <div class="col-auto my-auto px-1">
+                <div class="col-6 col-lg-auto my-auto px-1">
                     <div class="form-check">
                         <input class="form-check-input" type="checkbox" value="" id="asset-hide-zero">
                         <label class="form-check-label" for="asset-hide-zero">
