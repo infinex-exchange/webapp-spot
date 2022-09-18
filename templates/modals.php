e7b3e154aa4d6bb8f1c43b0f6ccf07fc6418b21d
vayamos-exchange <admin@vayamos.cc>
2022-09-18 14:22:26 +0200

    Banner html

--- templates/modals.php
+++ templates/modals.php
@@ -14,4 +14,19 @@
         </div>
     </div>
 </div>
+<div class="modal fade" tabindex="-1" role="dialog" id="modal-banner">
+    <div class="modal-dialog modal-dialog-centered" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title"></h5>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body">
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="modal-close btn btn-secondary" data-bs-dismiss="modal">Close</button>
+            </div>
+        </div>
+    </div>
+</div>
 <script src="/js/modals.js?<?php echo filemtime(__DIR__.'/../js/modals.js'); ?>"></script>
