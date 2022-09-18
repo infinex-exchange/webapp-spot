c483984b1aaa33326386f50c7858de7c9068dd90
vayamos-exchange <admin@vayamos.cc>
2022-09-18 14:35:23 +0200

    Banner

--- js/modals.js
+++ js/modals.js
@@ -17,3 +17,33 @@ function msgBoxNoConn(redirect = false) {
     if(redirect) msgBoxRedirect('No connection');
     else msgBox('No connection');
 }
+
+$(document).on('renderingComplete', function() {
+    $.ajax({
+        url: config.apiUrl + '/info/banner',
+        type: 'POST',
+        data: JSON.stringify({}),
+        contentType: "application/json",
+        dataType: "json",
+    })
+    .retry(config.retry)
+    .done(function (data) {
+        if(data.success && data.active) {
+            var bannerId = localStorage.getItem('bannerId');
+            var bannerTimestamp = localStorage.getItem('bannerTimestamp');
+                
+            if(bannerId === null ||
+               bannerTimestamp === null ||
+               bannerTimestamp > Date.now() - 86400000 ||
+               bannerId != data.bannerid
+            ) {
+                localStorage.setItem("bannerId", data.bannerId);
+                localStorage.setItem("bannerTimestamp", Date.now());
+                
+                var modal = $('.modal-banner');
+                modal.find('.modal-title').html(data.title);
+                modal.find('.modal-body').html(data.body);
+            }
+        }
+    });
+});
