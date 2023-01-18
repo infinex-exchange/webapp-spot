ab992997a0dd406d52f96e1a4678c86474f1a425
infinex-exchange <admin@infinex.cc>
2023-01-18 15:16:20 +0100

    Flask icon spot styling

--- exchange/spot/index.php
+++ exchange/spot/index.php
@@ -99,7 +99,7 @@
             
             <div class="col-3 ui-card ui-card-hor my-auto small" data-ui-card="trades chart orderbook orders">
                 <h4 class="primary ticker-name"></h4>
-                <i class="fa-solid fa-flask primary bg-red p-1 rounded"></i>
+                <i class="experimental fa-solid fa-flask primary bg-red p-1 rounded"></i>
                 <span class="ticker-base-name"></span>
             </div>
                 
@@ -204,6 +204,7 @@
             <!-- Header -->
             <div id="desktop-header" class="col-3 ui-card ui-card-ver ui-card-hor my-auto small d-none d-lg-block secondary">
                 <h4 class="primary ticker-name"></h4>
+                <i class="experimental fa-solid fa-flask primary bg-red p-1 rounded"></i>
                 <span class="ticker-base-name"></span>
             </div>
                 
