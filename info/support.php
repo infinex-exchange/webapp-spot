d6e671a6e0b567d226f9446380685d67732dd68b
infinex-exchange <admin@vayamos.cc>
2023-01-29 12:24:47 +0100

    Support layout

--- info/support.php
+++ info/support.php
@@ -27,13 +27,14 @@
             </div>
             
             <div class="row">
-                <div id="support-menu" class="col-12 p-2">
+                <div class="col-12 p-2">
                     <p>
                         This is the official Infinex support contact form. We do not respond to requests sent in any other way, like social media.
                         We'll respond to your request by e-mail, usually within 48 hours, excluding Saturdays and Sundays.
                         Please select the subject of your request.
                     </p>
-                    
+                </div>
+                <div id="support-menu" class="col-12 p-2">
                     <div class="list-group list-group-flush">
                         <button type="button" class="list-group-item list-group-item-action" data-goto="support-login" data-for="guest">
                             I'm having trouble with login or registration
@@ -51,25 +52,26 @@
                 </div>
                 
                 <div id="support-login" class="col-12 p-2">
-                
-                    <div class="row mt-4 mt-lg-5">
+                    <div class="row">
+                    
+                    
                         <div class="col-12 pb-1">
                             <h5 class="secondary">Your e-mail address:</h5>
                         </div>
                         <div class="col-12 col-lg-6">
                             <input id="sl-email" type="email" class="form-control">
                         </div>
-                    </div>
-                    
-                    <div class="row mt-4 mt-lg-5">
+    
+    
                         <div class="col-12 pb-1">
                             <h5 class="secondary">Please describe the problem:</h5>
                         </div>
                         <div class="col-12 col-lg-6">
                             <textarea id="sl-description" class="form-control"></textarea>
                         </div>
+                        
+                        
                     </div>
-                    
                 </div>
                 
                 <div class="col-12 p-2">
