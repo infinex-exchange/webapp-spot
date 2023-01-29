b20c12fddc9722271e5e40c983b97f7c3562929c
infinex-exchange <admin@vayamos.cc>
2023-01-29 13:56:50 +0100

    render withdrawals

--- info/support.js
+++ info/support.js
@@ -68,15 +68,19 @@ function renderWithdrawal(data) {
     var cTime = new Date(data.create_time * 1000).toLocaleString();
     
     return `
-        <div class="row hoverable withdrawal-item small px-1 py-2">
+        <div class="row hoverable withdrawal-item small p-1">
 
-            <div class="col-3">
+            <div class="col-2">
                 ${cTime}
             </div>
             
-            <div class="col-3">
+            <div class="col-2">
                 ${data.amount}
                 ${data.asset}
+            </div>
+            
+            <div class="col-2">
+                ${data.network_description}
             </div>
             
             <div class="col-6 text-break">
